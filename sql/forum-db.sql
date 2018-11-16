872a3cdbded53d0df8b39e88892fc3ea6f5fd6e8
senan0144 <30856990+senan0144@users.noreply.github.com>
2018-11-16 19:53:47 +0400

    new sql added

--- sql/forum-db.sql
+++ sql/forum-db.sql
@@ -58,7 +58,7 @@ CREATE TABLE `comment` (
   KEY `fk_comment_topic_idx` (`id_topic`),
   CONSTRAINT `fk_comment_topic` FOREIGN KEY (`id_topic`) REFERENCES `topic` (`id_topic`) ON DELETE CASCADE,
   CONSTRAINT `fk_comment_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +67,7 @@ CREATE TABLE `comment` (
 
 LOCK TABLES `comment` WRITE;
 /*!40000 ALTER TABLE `comment` DISABLE KEYS */;
-INSERT INTO `comment` VALUES (1,'Typography helps you engage your audience and establish a distinct, unique personality on your website. Knowing how to use fonts to build character in your design is a powerful skill, and exploring the history and use of typefaces, as well as typogra...','2018-10-21 09:00:00',1,5),(2,'Thx for answer. But still got problems. Updated manifest and changed onStart to onStartCommand but does not override now','2018-10-21 09:11:00',1,3),(3,'Thx for answer. ','2018-10-21 10:15:00',1,1),(4,'Due to the difference in the power they are able to draw, phones will always be an order of magnitude slower than desktops. However, don \'t give up on Python for the mobile device, because typically only a fraction of the code is responsible for the processor-intensive work, and this fraction can be optimised by rewriting it in another language.','2018-09-17 16:23:00',3,1),(22,'Okay I\'ll try!','2018-11-09 13:59:46',1,3);
+INSERT INTO `comment` VALUES (1,'Typography helps you engage your audience and establish a distinct, unique personality on your website. Knowing how to use fonts to build character in your design is a powerful skill, and exploring the history and use of typefaces, as well as typogra...','2018-10-21 09:00:00',1,5),(2,'Thx for answer. But still got problems. Updated manifest and changed onStart to onStartCommand but does not override now','2018-10-21 09:11:00',1,3),(3,'Thx for answer. ','2018-10-21 10:15:00',1,1),(4,'Due to the difference in the power they are able to draw, phones will always be an order of magnitude slower than desktops. However, don \'t give up on Python for the mobile device, because typically only a fraction of the code is responsible for the processor-intensive work, and this fraction can be optimised by rewriting it in another language.','2018-09-17 16:23:00',3,1),(22,'Okay I\'ll try!','2018-11-09 13:59:46',1,3),(23,'Thanks! I\'ll try..','2018-11-10 22:24:31',3,3),(24,'learn another language','2018-11-10 22:26:26',3,3);
 /*!40000 ALTER TABLE `comment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -136,11 +136,12 @@ CREATE TABLE `topic` (
   `description` text,
   `share_date` datetime DEFAULT NULL,
   `view_count` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
   `id_user` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_topic`),
   KEY `fk_topic_user_idx` (`id_user`),
   CONSTRAINT `fk_topic_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -149,7 +150,7 @@ CREATE TABLE `topic` (
 
 LOCK TABLES `topic` WRITE;
 /*!40000 ALTER TABLE `topic` DISABLE KEYS */;
-INSERT INTO `topic` VALUES (1,'10 Kids Unaware of Their Halloween Costume','Today, we re looking at three particularly interesting stories. Pinterest added a new location-based feature on Wednesday that uses Place Pins as a way to map out vacations and favorite areas. Southwest Airlines is providing Wi-Fi access from gate to gate for $8 per day through an onboard hotspot. And in an effort to ramp up its user base, Google Wallet is offering a debit card that can take out cash from.','2018-10-21 03:00:00',304,1),(2,'josso integration with spring web srevices','I require help on integration of web service with JOSSO for authentication. Whenever a request comes from SOAP UI(client) to service, it should be authenticated against an Active Directory server, and if it succeeds then it should hit the service endpoint.','2018-10-20 08:15:00',333,3),(3,'Unusual Sign In notification with Spring Security','Some popular websites send email/push notifications to their users if they notice unusual or suspicious login activity. Examples include Google, Facebook, Mozilla, Booking.com. How to implement this feature in Java/Spring application?','2018-09-15 05:30:00',193,4),(12,'How to fix Maven, Maven Configuration and Maven Dependency Problems in Eclipse','I went to directory of my project MainDirectory/MyProject and I run the same command, once again it worked well.I run Eclipse and I wanted to import \"Existing Maven Projects\". I chose MainDirectory as RootDirectory and I got a warning.','2018-11-09 14:01:04',0,3);
+INSERT INTO `topic` VALUES (1,'10 Kids Unaware of Their Halloween Costume','Today, we re looking at three particularly interesting stories. Pinterest added a new location-based feature on Wednesday that uses Place Pins as a way to map out vacations and favorite areas. Southwest Airlines is providing Wi-Fi access from gate to gate for $8 per day through an onboard hotspot. And in an effort to ramp up its user base, Google Wallet is offering a debit card that can take out cash from.','2018-10-21 03:00:00',310,1,1),(2,'josso integration with spring web srevices','I require help on integration of web service with JOSSO for authentication. Whenever a request comes from SOAP UI(client) to service, it should be authenticated against an Active Directory server, and if it succeeds then it should hit the service endpoint.','2018-10-20 08:15:00',334,1,3),(3,'Unusual Sign In notification with Spring Security','Some popular websites send email/push notifications to their users if they notice unusual or suspicious login activity. Examples include Google, Facebook, Mozilla, Booking.com. How to implement this feature in Java/Spring application?','2018-09-15 05:30:00',197,1,4),(12,'How to fix Maven, Maven Configuration and Maven Dependency Problems in Eclipse','I went to directory of my project MainDirectory/MyProject and I run the same command, once again it worked well.I run Eclipse and I wanted to import \"Existing Maven Projects\". I chose MainDirectory as RootDirectory and I got a warning.','2018-11-09 14:01:04',12,1,3);
 /*!40000 ALTER TABLE `topic` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -162,18 +163,19 @@ DROP TABLE IF EXISTS `user`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `user` (
   `id_user` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(45) DEFAULT NULL,
-  `password` varchar(45) DEFAULT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
   `token` varchar(100) DEFAULT NULL,
   `status` int(11) DEFAULT NULL,
   `id_role` int(11) DEFAULT NULL,
   `first_name` varchar(45) DEFAULT NULL,
   `last_name` varchar(45) DEFAULT NULL,
+  `image` text,
   PRIMARY KEY (`id_user`),
   UNIQUE KEY `email_UNIQUE` (`email`),
   KEY `fk_user_role_idx` (`id_role`),
   CONSTRAINT `fk_user_role` FOREIGN KEY (`id_role`) REFERENCES `role` (`id_role`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -182,7 +184,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'elvin@gmail.com','1234','123456',1,2,'Elvin','Xalafov'),(3,'senan@gmail.com','1234','123456',1,2,'Senan','Kazimov'),(4,'fatime@gmail.com','1234','123456',1,2,'Fatime','Gurbanova'),(5,'vuqar@gmail.com','1234','123456',1,2,'Vuqar','Nesirov'),(16,'senan0144@gmail.com','WM2wmqp/RW55f/Ljs7IYmg==','2d39e56e-d4fc-471c-a648-8cc2eab8678b',2,2,'senan','kazimov');
+INSERT INTO `user` VALUES (1,'elvin@gmail.com','WM2wmqp/RW55f/Ljs7IYmg==','123456',1,2,'Elvin','Xalafov','default.png'),(3,'senan@gmail.com','WM2wmqp/RW55f/Ljs7IYmg==','123456',1,2,'Senan','Kazimov','default.png'),(4,'fatime@gmail.com','WM2wmqp/RW55f/Ljs7IYmg==','123456',1,2,'Fatime','Gurbanova','default.png'),(5,'vuqar@gmail.com','WM2wmqp/RW55f/Ljs7IYmg==','123456',1,2,'Vuqar','Nesirov','default.png'),(28,'senan0144@gmail.com','WM2wmqp/RW55f/Ljs7IYmg==','911d3b98-1426-4fc0-957a-93d72fa4f9b8',1,2,'Senan','Kazimov','senan0144@gmail.com\\boy.png');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -195,4 +197,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-09 21:28:20
+-- Dump completed on 2018-11-16 19:51:38
